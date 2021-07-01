f77c37ab7412fae8bf6a639f8ca94c5d18bb7e82
pedrocamet <cametp@gmail.com>
2021-06-30 21:48:47 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath));
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+});
+
+
+app.listen(3001, function() {
+    console.log("Servidor corriendo");
+});
+
+app.get('/login', (req,res) =>{
+    res.send("log in")
+ }); // Pagina de Log In
+
---
+++ retro.md
@@ -0,0 +1,4 @@
+Sprint 1 : creamos repositorio, subimos wireframe y creamos archivo Readme, vinculamos a todos en el github.
+Hubo buena predisposicion de todos y trabajo en equipo pero los tiempos de entrega podrian haber sido mejores, y el trabajo en equipo de manera independiente y no en simultaneo puede ser mas eficiente.
+
+Sprint 2 :  
